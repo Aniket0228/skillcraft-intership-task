b3fbebc94f79b62697b7104df9c3c525de00ded4
Aniket mate <aniketmate1717@gmail.com>
2026-02-04 19:42:58 +0530

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const navbar = document.getElementById("navbar");
-
-window.addEventListener("scroll", () => {
-  navbar.classList.toggle("scrolled", window.scrollY > 60);
-});
-document.getElementById("year").textContent = new Date().getFullYear();
